62a3e517c927b18c1fbd4d5d0c85d8bf03f6f048
pswbuild <pswbuild@oppo.com>
2022-07-25 20:06:37 +0800

    Synchronize codes for Oppo Pad Air

---
+++ vendor/oplus/kernel/system/include/oplus_misc.h
@@ -0,0 +1,8 @@
+#ifndef OPLUS_MISC_H
+#define OPLUS_MISC_H
+
+extern void oppo_switch_fulldump(int on);
+extern int dsi_panel_backlight_get(void);
+extern void send_sig_to_get_trace(char *name);
+
+#endif
---
+++ vendor/oplus/kernel/system/include/proc.h
@@ -0,0 +1,7 @@
+#ifndef _SELINUX_PROC_H_
+#define _SELINUX_PROC_H_
+
+int is_avc_audit_enable(void);
+int init_denied_proc(void);
+
+#endif /* _SELINUX_PROC_H_ */
